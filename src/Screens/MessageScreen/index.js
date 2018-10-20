967c1fb53ff9386351e92923dc2eceb55e500e8a
turcurudin <ivdadrelbul@gmail.com>
2018-10-20 13:30:40 -0400

    Added my code for text input box with styling.

--- src/Screens/MessageScreen/index.js
+++ src/Screens/MessageScreen/index.js
@@ -1,20 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, KeyboardAvoidingView, Alert, Keyboard } from 'react-native';
 import { Permissions, SMS, Contacts, AuthSession } from 'expo';
+import { Header } from 'react-navigation';
 
 export default class MessageScreen extends React.Component {
 
   constructor() {
     super();
     this.state = {
+        message: ''
     }
+  }
+  
+  onSubmitButton(event){
+    Alert.alert('Sent!', this.state.message) //creates an alert using the text you entered.
+    this.textInput.clear() //Clears your sent message from the textInput.
+    Keyboard.dismiss() //closes the keyboard while you aren't typing.
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hi from messages</Text>
+      <KeyboardAvoidingView 
+        keyboardVerticalOffset = {Header.HEIGHT + 20}
+        style={styles.mainWindow} behavior="padding">
+        
+        <View style={styles.container}>
+            <Text>Hi from messages</Text>
+        </View>
+      <View style={styles.textEntry}>
+            <TextInput
+                style={{padding: 10, flex: 2}}
+                underlineColorAndroid="transparent"
+                multiline={true}
+                placeholder="Type here!"
+                onChangeText={(value) => this.setState({message: value})}
+                ref={input => { this.textInput = input }}
+                    value={this.state.message}
+            />
+            <View style={{flexDirection: 'column', justifyContent: 'center'}}>
+                <Button
+                  
+                  onPress={this.onSubmitButton.bind(this)}
+                    
+                  title="Send"
+                />
+            </View>
       </View>
+    </KeyboardAvoidingView>
     );
   }
 }
@@ -25,5 +57,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mainWindow: {
+      flex: 1, 
+      flexDirection: 'column', 
+      justifyContent: 'flex-end'
+  },
+  textEntry: {
+    backgroundColor: 'aliceblue', 
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    padding: 5
   },
 });
